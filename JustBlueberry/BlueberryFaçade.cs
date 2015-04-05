3000d183bae4c1a5bc5b5c2b8121cd8f80bc5b98
Васил Динев <vassildinev@gmail.com>
2015-04-05 10:21:01 +0300

    Added façade.

---
+++ JustBlueberry/BlueberryFaçade.cs
@@ -0,0 +1,31 @@
+﻿namespace JustBlueberry
+{
+    using System.Collections.Generic;
+
+    using JustBlueberry.Blueberries.Contracts;
+    using JustBlueberry.Common;
+    using JustBlueberry.Engine;
+    using JustBlueberry.Engine.Operation;
+    using JustBlueberry.Factory;
+    using JustBlueberry.Operator;
+    using JustBlueberry.Particles;
+    using JustBlueberry.Renderer;
+
+    public static class BlueberryFaçade
+    {
+        public static void StartApplication()
+        {
+            var renderer = new AdvancedConsoleRenderer(GlobalConstants.DefaultWorldRows, GlobalConstants.DefaultWorldCols);
+
+            renderer.RenderWelcomeScreen();
+
+            var particleOperator = new ParticleOperator();
+
+            var operationStrategy = new BlueberryOperationStrategy(particleOperator);
+
+            var engine = new BlueberryEngine(renderer, particleOperator, operationStrategy, 50);
+
+            engine.Run();
+        }
+    }
+}
